7447190a3bb94df3079de2f53e44746e46f06103
VRevathy <83732603+VRevathy@users.noreply.github.com>
2024-04-06 01:36:42 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -23,7 +23,7 @@ st.write('The name on your smoothie will be:', name_on_order)
 
 
 # Display the Fruit Options List in Your Stramlit in Snowflake(Sis) APP
-session = get_active_session()
+#session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
