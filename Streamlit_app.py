33b87a37c536c127cc4e9ee42ee1d175415f3d65
VRevathy <83732603+VRevathy@users.noreply.github.com>
2024-04-06 01:56:01 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -47,6 +47,8 @@ if ingredients_list:
 #To convert the LIST to a STRING we can add an FOR LOOP block. A FOR LOOP will repeat once FOR every value in the LIST.
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values('""" + ingredients_string + """','""" + name_on_order + """')"""
 
@@ -73,8 +75,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!' , icon="✅")
 
-# New Section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
+
